844a08054fc64bc6dba1a02d922bf43b9b83051d
asd <isaque.ferreira@dingo.pt>
2022-08-13 18:40:05 -0400

    dd-128 test used cypress

--- cashIn.py
+++ cashIn.py
@@ -1,11 +1,20 @@
 from request_graphql import getInfoDef, setOrders, setWrongAuthorized, getPlanInvoice
-import datetime
+from datetime import datetime
+import json
+with open('env.json') as json_file:
+    envFile = json.load(json_file)
 
 
 def workCashIn(mt5, local, local_mt):
     for item in getPlanInvoice(local):
         print(item['PlanInvoices'])
         print(item['AccountMetaTrader'])
+        print('PlanInvoices=>',
+              (item['PlanInvoices']['beginDate']).split('T')[0])
+        from_date = datetime.strptime(
+            (item['PlanInvoices']['beginDate']).split('T')[0], "%Y-%m-%d")
+        to_date = datetime.strptime(
+            (item['PlanInvoices']['finishDate']).split('T')[0], "%Y-%m-%d")
 
         authorized = mt5.initialize(
             path=r''+local_mt,
@@ -15,10 +24,11 @@ def workCashIn(mt5, local, local_mt):
             timeout=25000
         )
 
-        from_date = datetime.strptime(item['PlanInvoices']['beginDate'])
-        to_date = datetime.strptime(item['PlanInvoices']['finishDate'])
         history_orders = mt5.history_orders_total(from_date, to_date)
         if history_orders > 0:
             print("Total history orders=", history_orders)
         else:
             print("Orders not found in history")
+
+
+workCashIn('mt5', envFile['addressApi'], 'local_mt')
